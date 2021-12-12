1776c43e1370733d87fa3b984bffc8787288cc52
Yaraslau Borzdy <92574019+Frameholder@users.noreply.github.com>
2021-12-13 00:16:25 +0100

    Update Module_15.py

--- Module_15.py
+++ Module_15.py
@@ -2,10 +2,33 @@ Myfile=input('Enter file name ')
 Myfile+=".txt"
 with open(Myfile) as f:
     fileText = f.read()
-   # print(fileText)
+   #print(fileText)
 def changeText(fileText):
-    for i in '!"\'#$%&()*+-,/:;<=>?@[\\]^_{|}~':
+    for i in '!"\'#$%&()*+-,--/:.;<=>?@[\\]^_{|}~':
         fileText = fileText.replace(i, '')
-
     return fileText.split()
-print(changeText(fileText))
+
+NewScript=changeText(fileText)
+
+print(NewScript)#Выводим разделенные слова из текста в виде списка
+
+def mostCommon(NewScript, lenght=0):
+    most_common = []
+    qty_most_common = 0
+    for item in NewScript:
+        if len(item) > lenght:
+            qty = NewScript.count(item)
+        if qty > qty_most_common and qty > 3:
+            qty_most_common = qty
+            most_common = [item]
+        elif qty == qty_most_common:
+            most_common.append(item)
+    return list(set(most_common))
+Result=mostCommon(NewScript)
+print('Самое частое слово больше трех символов - это ', "".join(map(str,Result)))
+print('Самое длинное слово  - это ', max(NewScript, key=len))
+
+
+
+
+
